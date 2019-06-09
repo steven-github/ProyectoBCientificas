ee621553c4256a7e04a765ad09a9c8253a72d621
Connorturok27 <josuecjmm@gmail.com>
2019-06-09 16:44:01 -0600

    Inital Bundle Config and RouteConfig configs Signed-off-by: Connorturok27 <josuecjmm@gmail.com>

--- ProyectoBitacorasCientificas/App_Start/BundleConfig.cs
+++ ProyectoBitacorasCientificas/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace ProyectoBitacorasCientificas
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
