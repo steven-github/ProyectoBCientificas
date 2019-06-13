16ec05e8541011af2c9749f754c408d26ddbb5ef
Steven S <freelancers2010@gmail.com>
2019-06-12 23:08:51 -0600

    Adding Controllers + Views

---
+++ ProyectoBitacorasCientificas/Controllers/AdministracionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoBitacorasCientificas.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        // GET: Administracion
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Administracion/RamasCientificas
+        public ActionResult RamasCientificas()
+        {
+            return View();
+        }
+        // GET: Administracion/Proyectos
+        public ActionResult Proyectos()
+        {
+            return View();
+        }
+        // GET: Administracion/BitacorasCientificas
+        public ActionResult BitacorasCientificas()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoBitacorasCientificas/Controllers/ConsultaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoBitacorasCientificas.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        // GET: Consulta
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Consulta/Bitacora
+        public ActionResult Bitacora()
+        {
+            return View();
+        }
+        // GET: Consulta/Errores
+        public ActionResult Errores()
+        {
+            return View();
+        }
+        // GET: Consulta/Descargas
+        public ActionResult Descargas()
+        {
+            return View();
+        }
+    }
+}
