c8a6e438f9e380f4c5c3dca237db098657417778
Connorturok27 <josuecjmm@gmail.com>
2019-07-09 19:37:08 -0600

    Set the List for Ramas Cientificas , Proyectos Signed-off-by: Connorturok27 <josuecjmm@gmail.com>

---
+++ ProyectoBitacorasCientificas/ViewModels/ProyectoRamaCientificaForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProyectoBitacorasCientificas.Models;
+
+namespace ProyectoBitacorasCientificas.ViewModels
+{
+    public class ProyectoRamaCientificaForm
+    {
+        public IEnumerable<RamaCientifica> RamasCientificas { get; set; }
+        public Proyectos Proyectos { get; set; }
+    }
+}
