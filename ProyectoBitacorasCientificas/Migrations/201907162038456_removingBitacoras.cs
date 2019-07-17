a8258aae5737cb71198f18ede7396401fc08dd55
Connorturok27 <josuecjmm@gmail.com>
2019-07-17 16:57:48 -0600

    Modifying project for Bitacoras Signed-off-by: Connorturok27 <josuecjmm@gmail.com>

---
+++ ProyectoBitacorasCientificas/Migrations/201907162038456_removingBitacoras.cs
@@ -0,0 +1,34 @@
+namespace ProyectoBitacorasCientificas.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class removingBitacoras : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Bitacora", "Proyectos_id", "dbo.Proyectos");
+            DropIndex("dbo.Bitacora", new[] { "Proyectos_id" });
+            DropTable("dbo.Bitacora");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Bitacora",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        prefijo = c.String(),
+                        nombre = c.String(),
+                        fecha = c.DateTime(),
+                        ProyectoId = c.Int(nullable: false),
+                        Proyectos_id = c.Int(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateIndex("dbo.Bitacora", "Proyectos_id");
+            AddForeignKey("dbo.Bitacora", "Proyectos_id", "dbo.Proyectos", "id");
+        }
+    }
+}
---
+++ ProyectoBitacorasCientificas/Models/Bitacora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBitacorasCientificas.Models
+{
+    public class Bitacora
+    {
+        public int id { get; set; }
+        public string prefijo { get; set; }
+        public string nombreExperimento { get; set; }
+        public string Fecha { get; set; }
+        public Proyectos Proyectos { get; set; }
+        public int ProyectosId { get; set; }
+    }
+}
